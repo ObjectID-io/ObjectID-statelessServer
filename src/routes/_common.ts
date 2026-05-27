4531c7053a3efa35153c71c0e2049559398f7994
Lucas J. González <lgonzalez@opentia.net>
2026-05-27 10:59:11 +0200

    fix: upgraded setupEnv and packageID for get_OID_controllerCap

--- src/routes/_common.ts
+++ src/routes/_common.ts
@@ -15,6 +15,7 @@ export type SetupEnvResult = {
   documentPackageID: string;
   gs1PackageId: string;
   gs1RegistryId: string;
+  creditPackageID: string;
   storagePackageID: string;
   storageCreditPackageID: string;
   storagePolicyID: string;
@@ -149,6 +150,7 @@ export async function setupEnv(seed: string, network: string): Promise<SetupEnvR
     documentPackageID,
     gs1PackageId,
     gs1RegistryId,
+    creditPackageID,
     storagePackageID,
     storageCreditPackageID,
     storagePolicyID,
